2926efb77fcf04212c29b0eb30ebc7ebce31a0c3
Alexia <alexialeong5@gmail.com>
2023-09-15 18:16:16 -0400

    Added to lab1.summary files for rest of lab1

--- lab1_data.R
+++ lab1_data.R
@@ -1,18 +1,16 @@
 library(XLS)
 library(readxl)
-EPI_data <- read.csv("../2010EPI_data.csv")
+#EPI_data <- read.csv("../2010EPI_data.csv")
 
+#or
+EPI_data <- read_excel("../2010EPI_data.xls", sheet = "EPI2010_onlyEPIcountries")
+# Note: replace default data frame name – cannot start with numbers!
 names(EPI_data) <- as.matrix(EPI_data[1, ])
 EPI_data <- EPI_data[-1, ]
 EPI_data[] <- lapply(EPI_data, function(x)
   type.convert(as.character(x)))
 EPI_data
 View(EPI_data)
-
-#or
-#EPI_data <- read.xls(”../2010EPI_data.xls")
-# Note: replace default data frame name – cannot start with numbers!
-#View(EPI_data)
 #
 attach(EPI_data) 	# sets the ‘default’ object
 fix(EPI_data) 	# launches a simple data editor
--- lab1_summary.R
+++ lab1_summary.R
@@ -11,3 +11,62 @@ help(rug)
 rug(EPI) 
 
 plot(ecdf(EPI), do.points=FALSE, verticals=TRUE) 
+
+par(pty="s")
+qqnorm(EPI); qqline(EPI)
+
+x<-seq(30,95,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for tdsn")
+qqline(x)
+
+##Same exploration for other 2 variables: Biodiversity and FORESTRY
+BIODIVERSITY
+summary(BIODIVERSITY)
+fivenum(BIODIVERSITY,na.rm=TRUE)
+stem(BIODIVERSITY)
+hist(BIODIVERSITY)
+help("seq")
+hist(BIODIVERSITY, seq(0., 100., 1.0), prob=TRUE)
+lines(density(BIODIVERSITY,na.rm=TRUE,bw=1.)) # or try bw=“SJ”
+rug(BIODIVERSITY)
+plot(ecdf(BIODIVERSITY), do.points=FALSE, verticals=TRUE) 
+
+par(pty="s")
+qqnorm(BIODIVERSITY); qqline(BIODIVERSITY)
+
+x<-seq(0,100,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for tdsn")
+qqline(x)
+
+FORESTRY
+summary(FORESTRY)
+fivenum(FORESTRY,na.rm=TRUE)
+stem(FORESTRY)
+hist(FORESTRY)
+hist(FORESTRY, seq(0., 100., 1.), prob=TRUE)
+lines(density(FORESTRY,na.rm=TRUE,bw=1.)) # or try bw=“SJ”
+rug(FORESTRY)
+plot(ecdf(FORESTRY), do.points=FALSE, verticals=TRUE) 
+
+par(pty="s")
+qqnorm(FORESTRY); qqline(FORESTRY)
+
+x<-seq(0,100,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for tdsn")
+
+boxplot(EPI,DALY)
+qqplot(EPI,DALY)
+
+boxplot(EPI,
+        ENVHEALTH, ECOSYSTEM, DALY, AIR_H,
+        WATER_H, AIR_E, WATER_E,
+        BIODIVERSITY)
+
+EPILand<-EPI[!Landlock]
+ELand <- EPILand[!is.na(EPILand)]
+hist(ELand)
+hist(ELand, seq(30., 95., 1.0), prob=TRUE)
+
+EPI_South_Asia <- EPI[EPI_regions == "South Asia"]
+E_South_Asia <- EPI_South_Asia[!is.na(EPI_South_Asia)]
+hist(E_South_Asia)
