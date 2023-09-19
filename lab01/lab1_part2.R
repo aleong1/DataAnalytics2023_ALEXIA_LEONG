86d3e7d5b717eede76234dde08e14ee947a660c2
Alexia <alexialeong5@gmail.com>
2023-09-19 17:08:14 -0400

    Added to lab1 part 2

--- lab01/lab1_part2.R
+++ lab01/lab1_part2.R
@@ -47,3 +47,52 @@ qplot(mtcars$cyl)
 qplot(factor(mtcars$cyl))
 
 #Bar graph of counts
+
+qplot(factor(cyl), data = mtcars)
+ggplot(mtcars,aes(x=factor(cyl))) + geom_bar()
+
+#Creating histograms
+hist(mtcars$mpg)
+hist(mtcars$mpg, breaks = 10)
+hist(mtcars$mpg, breaks = 5)
+hist(mtcars$mpg, breaks = 12)
+qplot(mpg, data = mtcars, binwidth = 4)
+ggplot(mtcars,aes(x=mpg)) + geom_histogram(binwidth = 4)
+ggplot(mtcars, aes(x=mpg)) + geom_histogram(binwidth = 5)
+
+#Creating box plot
+plot(ToothGrowth$supp, ToothGrowth$len)
+boxplot(len ~ supp, data = ToothGrowth)
+boxplot(len ~ supp + dose, data = ToothGrowth)
+qplot(ToothGrowth$supp, ToothGrowth$len, geom = "boxplot")
+qplot(supp, len, data = ToothGrowth, geom = "boxplot")
+ggplot(ToothGrowth, aes(x=supp, y=len)) + geom_boxplot()
+qplot(interaction(ToothGrowth$supp, ToothGrowth$dose), ToothGrowth$len, geom = "boxplot")
+qplot(interaction(supp,dose), len, data = ToothGrowth, geom = "boxplot")
+ggplot(ToothGrowth, aes(x = interaction(supp, dose), y = len)) + geom_boxplot()
+
+
+# ggplot exercise
+df <- data.frame(
+  gp = factor(rep(letters[1:3], each = 10)),
+  y = rnorm(30)
+)
+ds <- plyr::ddply(df, "gp", plyr::summarise, mean = mean(y), sd = sd(y))
+
+ggplot(df, aes(gp, y)) +
+  geom_point() +
+  geom_point(data = ds, aes(y = mean), colour = 'red', size = 3)
+
+ggplot(df) +
+  geom_point(aes(gp, y)) +
+  geom_point(data = ds, aes(gp, mean), colour = 'red', size = 3)
+
+ggplot() +
+  geom_point(data = df, aes(gp, y)) +
+  geom_point(data = ds, aes(gp, mean), colour = 'red', size = 3) +
+  geom_errorbar(
+    data = ds,
+    aes(gp, mean, ymin = mean - sd, ymax = mean + sd),
+    colour = 'red',
+    width = 0.4
+  )
