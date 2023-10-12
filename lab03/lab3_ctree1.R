28bb974808eec7ddbe605522bc31babfbb5e2892
Alexia <alexialeong5@gmail.com>
2023-10-12 16:56:47 -0400

    worked on lab 3

--- lab03/lab3_ctree1.R
+++ lab03/lab3_ctree1.R
@@ -1,7 +1,7 @@
 require(rpart)
 Swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
-plot(swiss_rpart) # try some different plot options
-text(swiss_rpart) # try some different text options
+plot(Swiss_rpart) # try some different plot options
+text(Swiss_rpart) # try some different text options
 
 require(party)
 
