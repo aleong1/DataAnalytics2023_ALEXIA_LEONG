64a9baedcf33cdf764f1ab64fcf033c0c8d40cfa
Alexia <alexialeong5@gmail.com>
2023-11-14 11:32:02 -0500

    added group4 labs

--- group03/lab1_ctree1.R
+++ group03/lab1_ctree1.R
@@ -1,5 +1,7 @@
+setwd("C:/Users/AlexiaLeong.ALEXIARPI/OneDrive/Documents/Data Analytics/DataAnalytics2023_ALEXIA_LEONG/group03")
+
 require(rpart)
-Swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
 plot(swiss_rpart) # try some different plot options
 text(swiss_rpart) # try some different text options
 
